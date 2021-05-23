19cbfea29f24c4fef9306bf6b8c481f04c067de0
Juli-md <yjannalynnm@gmail.com>
2021-05-23 21:08:27 +1000

    fix linter problems

--- client/src/models/models.ts
+++ client/src/models/models.ts
@@ -1,27 +1,26 @@
 export interface IPlayerRow {
-    idNode: number;
-    fio: string;
-    level: string,
-    experience?: number;
-    money?: number;
-    place?: number
+  idNode: number;
+  fio: string;
+  level: string;
+  experience?: number;
+  money?: number;
+  place?: number;
 }
 
 export interface IPlayer {
-    idNode: number;
-    fio: string;
-    sex: string;
-    idParent: number;
-    idRole: number;
-    idZone: null;
-    idInstance: number;
-    level: string,
-    levelOrder: number;
-    resources: string;
+  idNode: number;
+  fio: string;
+  sex: string;
+  idParent: number;
+  idRole: number;
+  idZone: null;
+  idInstance: number;
+  level: string;
+  levelOrder: number;
+  resources: string;
 }
 
 export interface IResource {
-    resource: string;
-    value: number;
+  resource: string;
+  value: number;
 }
-
