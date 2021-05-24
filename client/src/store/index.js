72915c41a77d6567a922cadde258a89476bd8e76
Juli-md <yjannalynnm@gmail.com>
2021-05-24 10:45:01 +1000

    remove unnecessary files

--- client/src/store/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var vuex_1 = require("vuex");
-exports["default"] = vuex_1.createStore({
-    state: {},
-    mutations: {},
-    actions: {},
-    modules: {}
-});
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PlayersModule } from './players/players.module';
@@ -11,9 +9,7 @@ import { PlayersModule } from './players/players.module';
       rootPath: join(__dirname, '../..', 'client/dist'),
       exclude: ['/api*'],
     }),
-    PlayersModule
+    PlayersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
